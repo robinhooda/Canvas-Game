62a06f29569d51ebb20cb014e206cc7d774d4a6f
varunprabhakaran22 <50875768+varunprabhakaran22@users.noreply.github.com>
2019-09-18 12:05:08 +0530

    ball moving correctly

--- script.js
+++ script.js
@@ -1,38 +1,25 @@
-let canvas;
-let canvasContext;
+let canvas = document.getElementById("game");
+let canvasContext=canvas.getContext("2d");
 let ballRadius=10;
-let ballXPosition=400;
-let ballYPosition=470;
+let ballXPosition=canvas.width/2;
+let ballYPosition= canvas.height-30;
 let ballXSpeed=2;
 let ballYSpeed=-3;
 
-window.onload=function () {
-    canvas=document.getElementById('game');
-    canvasContext=canvas.getContext('2d');
-    canvasContext.fillStyle='#ff0005';
-    canvasContext.fillRect(0,0,canvas.width,canvas.height);
-
-    
-    
-    setInterval(() => {
-        clearCanvas();
-    }, 100);  
-}
-
 function drawBall() {
-
-    
-    canvasContext.fillStyle='#ff0005';
-    canvasContext.fillRect(0,0,canvas.width,canvas.height);
+    canvasContext.beginPath();
     canvasContext.fillStyle='#ffffff';
     canvasContext.arc(ballXPosition,ballYPosition, ballRadius, 0, 2 * Math.PI);
     canvasContext.fill();
+    canvasContext.closePath();
    
 }
 
 function clearCanvas(){
-    canvasContext.clearRect(0,0,canvas.width,canvas.height);
+    canvasContext.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
     ballXPosition=ballXPosition+ballXSpeed;
-    ballYPosition=ballYPosition+ballYSpeed;  
+    ballYPosition=ballYPosition+ballYSpeed;
 }
+
+setInterval(clearCanvas,100)
