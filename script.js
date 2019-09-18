2cedc717811fa87ede259cbb343cc7e6ce903702
varunprabhakaran22 <50875768+varunprabhakaran22@users.noreply.github.com>
2019-09-18 11:10:28 +0530

    movement

--- script.js
+++ script.js
@@ -3,37 +3,36 @@ let canvasContext;
 let ballRadius=10;
 let ballXPosition=400;
 let ballYPosition=470;
-let ballXSpeed=20;
-let ballYSpeed=4;
+let ballXSpeed=2;
+let ballYSpeed=-3;
 
 window.onload=function () {
     canvas=document.getElementById('game');
     canvasContext=canvas.getContext('2d');
+    canvasContext.fillStyle='#ff0005';
+    canvasContext.fillRect(0,0,canvas.width,canvas.height);
 
     
     
     setInterval(() => {
-        ballMovement();
-        drawBall();
-
-
-        
+        clearCanvas();
     }, 100);  
 }
 
-
-
-function ballMovement(){
-    ballXPosition=ballXPosition+ballXSpeed;
-    ballYPosition=ballYPosition+ballYSpeed;
-
-
-}
-
 function drawBall() {
+
+    
     canvasContext.fillStyle='#ff0005';
     canvasContext.fillRect(0,0,canvas.width,canvas.height);
     canvasContext.fillStyle='#ffffff';
     canvasContext.arc(ballXPosition,ballYPosition, ballRadius, 0, 2 * Math.PI);
     canvasContext.fill();
+   
+}
+
+function clearCanvas(){
+    canvasContext.clearRect(0,0,canvas.width,canvas.height);
+    drawBall();
+    ballXPosition=ballXPosition+ballXSpeed;
+    ballYPosition=ballYPosition+ballYSpeed;  
 }
